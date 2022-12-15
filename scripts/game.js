54a79e06753011c9ae47aa847b69cefbce24b863
ChrisLPlumb91 <chrislplumb91@hotmail.com>
2022-12-15 19:58:03 +0000

    Add rest of code

--- scripts/game.js
+++ scripts/game.js
@@ -3,7 +3,9 @@ let game = {
     currentGame: [],
     playerMoves: [],
     choices: ['button1', 'button2', 'button3', 'button4'],
-    turnNumber: 0
+    turnNumber: 0,
+    lastButton: "",
+    turnInProgress: false
 };
 
 function newGame() {
@@ -11,17 +13,25 @@ function newGame() {
     game.currentGame = [];
     game.playerMoves = [];
     game.turnNumber = 0;
+    lastButton = '';
+    turnInProgress = false;
         
     for (let circle of document.getElementsByClassName('circle')) {
         if (circle.getAttribute('data-listener') !== 'true') {
             circle.addEventListener('click', (e) => {
-                let move = e.target.getAttribute('id');
+                
+                if (game.currentGame.length > 0 && !game.turnInProgress) {
+                    let move = e.target.getAttribute('id');
 
-                lightsOn(move);
+                    game.lastButton = move;
 
-                game.playerMoves.push(move);
+                    lightsOn(move);
 
-                playerTurn();
+                    game.playerMoves.push(move);
+
+                    playerTurn();
+                }
+
             });
 
             circle.setAttribute('data-listener', 'true');
@@ -51,6 +61,8 @@ function lightsOn(circ) {
 }
 
 function showTurns() {
+    game.turnInProgress = true;
+
     game.turnNumber = 0;
 
     let turns = setInterval(() => {
@@ -59,8 +71,24 @@ function showTurns() {
 
         if (game.turnNumber >= game.currentGame.length) {
             clearInterval(turns);
+            game.turnInProgress = false;
         }
     }, 800);
 }
 
-module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
+function playerTurn() {
+    let i = game.playerMoves.length - 1;
+
+    if (game.currentGame[i] === game.playerMoves[i]) {
+        if (game.currentGame.length === game.playerMoves.length) {
+            game.score++;
+            showScore();
+            addTurn();
+        }
+    } else {
+        alert('Wrong move!');
+        newGame();
+    }
+}
+
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn };
